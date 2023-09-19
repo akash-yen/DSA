d1c295e3e8b6806ad8fd2d3cc7f6dca63de9d757
Akash <akashyenumula@gmail.com>
2023-09-19 13:48:17 -0400

    Modified bubble sort for optimization

--- GFG_DSA/Sorting.py
+++ GFG_DSA/Sorting.py
@@ -64,32 +64,54 @@ l = [89, 46, 69, 1, 37, 80,88,88, 64, 82, 87, 75]
 #         optimizer = optimizer +1
 #     return l
 # print(bubble_sort(l))
+#
+# # optimized code if given a sorted array
+# l = [1,2,3,4,5,6]
+# def bubble_sort(l):
+#     rounds = len(l) -1 # to run the while loop n-1 times
+#     optimizer = 1 # making less runs than ususal
+#     def check(l): # I defined another fucntion to check whether the given list is already a sorted list or not if soreted no need to run loops
+#         for i in range(len(l) - 1):
+#             if l[i] < l[i + 1]:
+#                 i = i + 1
+#             else:
+#                 break
+#         else:
+#             return l
+#     if check(l) == l:
+#         return l
+#     else:
+#         while rounds !=0 :
+#             for i in range(len(l)-optimizer):
+#                 if l[i] > l[i+1]:
+#                     l[i],l[i+1] =  l[i+1], l[i] #swaping the postions
+#                 else:
+#                     pass
+#             rounds = rounds -1
+#             optimizer = optimizer +1
+#         return l
+# print(bubble_sort(l))
 
-# optimized code if given a sorted array
-l = [1,2,3,4,5,6]
-def bubble_sort(l):
-    rounds = len(l) -1 # to run the while loop n-1 times
-    optimizer = 1 # making less runs than ususal
-    def check(l): # I defined another fucntion to check whether the given list is already a sorted list or not if soreted no need to run loops
-        for i in range(len(l) - 1):
-            if l[i] < l[i + 1]:
-                i = i + 1
-            else:
-                break
-        else:
-            return l
-    if check(l) == l:
-        return l
-    else:
-        while rounds !=0 :
-            for i in range(len(l)-optimizer):
-                if l[i] > l[i+1]:
-                    l[i],l[i+1] =  l[i+1], l[i] #swaping the postions
-                else:
-                    pass
-            rounds = rounds -1
-            optimizer = optimizer +1
-        return l
-print(bubble_sort(l))
+## Selection sort
+
+l = [89, 46, 69, 1, 37, 80,88,88, 64, 82, 87, 75]
+
+def selection_sort(l):
+    start = 0
+    l_min = l[start]
+    index = start
+    while start <= len(l)-1:
+        for i in l[start:]:
+            if i < l_min:
+                l_min = i
+                index = start +1
+        l[start] , l[index] = l[index],l[start]
+        start = start +1
+        index = start
+    print(l)
+selection_sort(l)
+
+
+
 
 
--- GFG_DSA/Test.py
+++ GFG_DSA/Test.py
@@ -1,11 +1,8 @@
-l = [1,2,3,4,5,6]
-def check(l):
-    for i in range(len(l)-1):
-        if l[i] < l[i+1]:
-            i=i+1
-        else:
-            break
-    else:
-        return l
-
-print (check(l))
+l = [89, 46, 69, 11, 37, 80,88,88, 64, 82, 87, 75]
+l_min = l[0]
+index = 0
+for i in range(len(l)-1):
+    if l[i] < l_min:
+        l_min = l[i]
+        index = i
+print(l_min,index)
