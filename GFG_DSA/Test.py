e9878f175237bc59ccfcb7f649d003cc0114d226
akashyenumula <akyenumula@gmail.com>
2023-09-20 00:27:25 -0400

    Sep-19th

--- GFG_DSA/Test.py
+++ GFG_DSA/Test.py
@@ -1,8 +1,28 @@
-l = [89, 46, 69, 11, 37, 80,88,88, 64, 82, 87, 75]
-l_min = l[0]
-index = 0
-for i in range(len(l)-1):
-    if l[i] < l_min:
-        l_min = l[i]
-        index = i
-print(l_min,index)
+# # l = [89, 46, 69, 11, 37,57,33,43,23,78,76,445]
+# l = [11,2,3,5,4]
+# index = 0
+# start = 0
+# while start < len(l)-1:
+#     l_min = l[start]
+#     index = 0
+#     for i in l:
+#         if i < l_min and i not in l[:start]:
+#             l_min = i
+#             swap = index
+#         index = index+1
+#         l[start],l[swap] = l[swap],l[start]
+#     start = start +1
+# print(swap)
+# print(l_min)
+# print(l)
+
+l = [1,2,3,7,4,8]
+str = 0
+min = l[str]
+while str < len(l)-1:
+    for i in l:
+        if i < min:
+            min = i
+    l[str] = min
+
+
